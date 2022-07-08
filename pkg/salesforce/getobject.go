fa218d5689f0807fec98968f478d7a06f81577a2
Ph33r <77519024+Ph33rr@users.noreply.github.com>
2022-07-09 01:51:58 +0300

    get write obj

--- pkg/salesforce/getobject.go
+++ pkg/salesforce/getobject.go
@@ -78,3 +78,24 @@ func GetObjectRecord(ResponseGET []byte, url string, foundEndPoint []string, rec
 	return data
 }
 
+func GetWritableObject(ResponseGET []byte, url string, foundEndPoint []string, objectname string, requestProxy string, requestHeaders []string) string {
+	var data string
+	payloadMassage := string(PayloadGeneratorWritableOBJ(objectname))
+	fwuid, app, markup := GetAuraContext(ResponseGET)
+	payloadAuraContext := string(PayloadGeneratorAuraContext(fwuid, app, markup))
+	requestMethod := "POST"
+	requestParameter := map[string]string{"message": payloadMassage, "aura.context": payloadAuraContext, "aura.token": "null"}
+
+	responsebyte := RequestSalesforcePOST(url+foundEndPoint[0], requestMethod, requestProxy, requestHeaders, requestParameter)
+	jsonparser.ArrayEach(responsebyte, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+		jsonvalu, _, _, err := jsonparser.Get(value, "error")
+		if err != nil {
+			log.Fatalf("An Error Occured %v", err)
+			gologger.Fatal().Msg("Failed to get ObjectWritable")
+		}
+		data = string(jsonvalu)
+
+	}, "actions")
+	return data
+}
+
