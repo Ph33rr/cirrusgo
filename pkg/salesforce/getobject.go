681f112eccf9aacbb57f1d2b2e4c63281227af42
Ph33r <77519024+Ph33rr@users.noreply.github.com>
2022-07-09 01:50:26 +0300

    items

--- pkg/salesforce/getobject.go
+++ pkg/salesforce/getobject.go
@@ -34,3 +34,26 @@ func GetObjectList(ResponseGET []byte, url string, foundEndPoint []string, reque
 	}, "actions")
 	return arrayObjList
 }
+
+
+func GetObjectItems(ResponseGET []byte, url string, foundEndPoint []string, objectName string, pageSize int, page int, requestProxy string, requestHeaders []string) string {
+	var data string
+	payloadMassage := string(PayloadGeneratorGetItems(objectName, pageSize, page))
+	fwuid, app, markup := GetAuraContext(ResponseGET)
+	payloadAuraContext := string(PayloadGeneratorAuraContext(fwuid, app, markup))
+	requestMethod := "POST"
+	requestParameter := map[string]string{"message": payloadMassage, "aura.context": payloadAuraContext, "aura.token": "null"}
+
+	responsebyte := RequestSalesforcePOST(url+foundEndPoint[0], requestMethod, requestProxy, requestHeaders, requestParameter)
+	jsonparser.ArrayEach(responsebyte, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+		jsonvalu, _, _, err := jsonparser.Get(value, "returnValue", "result")
+		if err != nil {
+			log.Fatalf("An Error Occured %v", err)
+			gologger.Fatal().Msg("Failed to get ObjectItems")
+		}
+		data = string(jsonvalu)
+
+	}, "actions")
+	return data
+}
+
