51318d5da589cd16728e39511c325c2eeb8dc1c4
Ph33r <77519024+Ph33rr@users.noreply.github.com>
2022-07-09 22:02:04 +0300

    fix dump

--- pkg/salesforce/getobject.go
+++ pkg/salesforce/getobject.go
@@ -46,7 +46,7 @@ func GetDump(ResponseGET []byte, url string, foundEndPoint []string, requestProx
 	requestParameter := map[string]string{"message": payloadMassage, "aura.context": payloadAuraContext, "aura.token": "null"}
 	responsebyte := RequestSalesforcePOST(url+foundEndPoint[0], requestMethod, requestProxy, requestHeaders, requestParameter)
 	jsonparser.ArrayEach(responsebyte, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
-		jsonvalu, _, _, err := jsonparser.Get(value, "returnValue", "apiNamesToKeyPrefixes")
+		jsonvalu, _, _, err := jsonparser.Get(value, "returnValue")
 		if err != nil {
 			log.Fatalf("An Error Occured %v", err)
 			gologger.Fatal().Msg("Failed to get ObjectList")
