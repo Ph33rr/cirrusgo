8ec5b2778673da6c8b0b1dfce8037f78addb05d4
Ph33r <77519024+Ph33rr@users.noreply.github.com>
2022-07-09 20:50:05 +0300

    Update getobject.go

--- pkg/salesforce/getobject.go
+++ pkg/salesforce/getobject.go
@@ -36,6 +36,31 @@ func GetObjectList(ResponseGET []byte, url string, foundEndPoint []string, reque
 }
 
 
+func GetDump(ResponseGET []byte, url string, foundEndPoint []string, requestProxy string, requestHeaders []string) []string {
+	var arrayObjList []string
+
+	payloadMassage := string(PayloadGeneratorDump())
+	fwuid, app, markup := GetAuraContext(ResponseGET)
+	payloadAuraContext := string(PayloadGeneratorAuraContext(fwuid, app, markup))
+	requestMethod := "POST"
+	requestParameter := map[string]string{"message": payloadMassage, "aura.context": payloadAuraContext, "aura.token": "null"}
+	responsebyte := RequestSalesforcePOST(url+foundEndPoint[0], requestMethod, requestProxy, requestHeaders, requestParameter)
+	jsonparser.ArrayEach(responsebyte, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+		jsonvalu, _, _, err := jsonparser.Get(value, "returnValue", "apiNamesToKeyPrefixes")
+		if err != nil {
+			log.Fatalf("An Error Occured %v", err)
+			gologger.Fatal().Msg("Failed to get ObjectList")
+		}
+		addValu := map[string]string{}
+		json.Unmarshal([]byte(jsonvalu), &addValu)
+		for k, _ := range addValu {
+			arrayObjList = append(arrayObjList, k)
+		}
+	}, "actions")
+	return arrayObjList
+}
+
+
 func GetObjectItems(ResponseGET []byte, url string, foundEndPoint []string, objectName string, pageSize int, page int, requestProxy string, requestHeaders []string) string {
 	var data string
 	payloadMassage := string(PayloadGeneratorGetItems(objectName, pageSize, page))
