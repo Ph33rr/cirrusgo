2d157fd35c553af22a026d98f965a32646e6558d
Ph33r <77519024+Ph33rr@users.noreply.github.com>
2022-07-10 08:41:16 +0300

    Update banner.go

--- internal/runner/banner.go
+++ internal/runner/banner.go
@@ -1,25 +1,15 @@
 package runner
 
+
 import (
 	"github.com/projectdiscovery/gologger"
+	"github.com/projectdiscovery/gologger/levels"
 )
 
-const banner = `
-   ______ _                           ______
-  / ____/(_)_____ _____ __  __ _____ / ____/____
- / /    / // ___// ___// / / // ___// / __ / __ \
-/ /___ / // /   / /   / /_/ /(__  )/ /_/ // /_/ /
-\____//_//_/   /_/    \__,_//____/ \____/ \____/ v0.0.1
-`
-
-// Version is the current version of CirrusGo
-const Version = `v0.0.1`
-
-// showBanner is used to show the banner to the user
 func showBanner() {
+	gologger.DefaultLogger.SetMaxLevel(levels.LevelDebug)
 	gologger.Print().Msgf("%s\n", banner)
 	gologger.Print().Msgf("\t\ttwitter: infosec_90\n\n")
-	gologger.Warning().Msgf("Use with caution. You are responsible for your actions\n")
-	gologger.Warning().Msgf("Developers assume no liability and are not responsible for any misuse or damage.\n")
-
+	gologger.Warning().Msg("Use with caution. You are responsible for your actions\n")
+	gologger.Warning().Msg("Developers assume no liability and are not responsible for any misuse or damage.\n")
 }
