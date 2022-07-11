19863dd847adb7af15b98a0b915607deb6bda2a1
Ph33r <77519024+Ph33rr@users.noreply.github.com>
2022-07-11 15:52:20 +0300

    Update salesforce.go

--- pkg/salesforce/salesforce.go
+++ pkg/salesforce/salesforce.go
@@ -2,8 +2,6 @@ package salesforce
 
 import (
 	"bytes"
-	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -11,10 +9,293 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/buger/jsonparser"
 	"github.com/ph33rr/CirrusGo/pkg/request"
+	"github.com/projectdiscovery/gologger"
 )
 
-type payload struct {
-	Massage json.RawMessage `json:"actions"`
+type dataSalesforce struct {
+	requestURL       string
+	requestMethod    string
+	requestProxy     string
+	requestHeaders   []string
+	requestParameter map[string]string
+}
+
+func RequestSalesforcePOST(requestURL string, requestMethod string, requestProxy string, requestHeaders []string, requestParameter map[string]string) []byte {
+
+	client := request.Client(requestProxy)
+	requestParam := url.Values{}
+
+	//add all param
+	if requestParameter != nil {
+
+		for keyParameter, valuParameter := range requestParameter {
+			requestParam.Set(keyParameter, valuParameter)
+		}
+	}
+	//encode all param
+	finalRequestBody := bytes.NewBuffer([]byte(requestParam.Encode()))
+	request, err := http.NewRequest(requestMethod, requestURL, finalRequestBody)
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	if err != nil {
+
+		log.Fatalf("An Error Occured %v", err)
+		gologger.Fatal().Msg("can't handle Add Header(POST)")
+
+	}
+
+	//add all header
+	if requestHeaders != nil {
+		for _, header := range requestHeaders {
+			parts := strings.SplitN(header, ":", 2)
+
+			if len(parts) != 2 {
+				continue
+			}
+
+			request.Header.Set(parts[0], parts[1])
+		}
+	}
+	//send request
+	response, err := client.Do(request)
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+		gologger.Fatal().Msg("Can't send POST request")
+
+	}
+
+	responseBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatalln(err)
+		gologger.Fatal().Msg("Can't handle response(POST)")
+	}
+
+	defer request.Body.Close()
+	//return respnse
+	return responseBody
+}
+
+//this func return response (fwuid and markup for salseforce)
+func RequestSalesforceGET(requestURL string, requestMethod string, requestProxy string, requestHeaders []string, requestParameter map[string]string) []byte {
+
+	client := request.Client(requestProxy)
+	requestParam := url.Values{}
+	//add param
+	if requestParameter != nil {
+
+		for keyParameter, valuParameter := range requestParameter {
+
+			requestParam.Set(keyParameter, valuParameter)
+		}
+	}
+	//encode param
+	finalRequestBody := bytes.NewBuffer([]byte(requestParam.Encode()))
+	request, err := http.NewRequest(requestMethod, requestURL, finalRequestBody)
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+		gologger.Fatal().Msg("can't handle Add Header(GET)")
+	}
+	//add header
+
+	if requestHeaders != nil {
+		for _, header := range requestHeaders {
+			parts := strings.SplitN(header, ":", 2)
+
+			if len(parts) != 2 {
+				continue
+			}
+
+			request.Header.Set(parts[0], parts[1])
+		}
+	}
+	//send request
+	response, err := client.Do(request)
+
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+
+		gologger.Fatal().Msg("Can't send GET request")
+	}
+
+	responseBody, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatalln(err)
+		gologger.Fatal().Msg("Can't handle response (GET)")
+	}
+
+	//check header if respnse.body == null follow redircet
+
+	checkResponseBody := string(responseBody)
+	//bytes.Equal(responseBody, emptyByteVar)
+	// not working for all website
+	//emptyByteVar := make([]byte, 128)
+
+	if strings.Contains(checkResponseBody, "fwuid") {
+		return responseBody
+
+	} else if len(checkResponseBody) == 0 {
+		// get url from Location header
+
+		responseHeader := response.Header.Get("Location")
+		requestURL := responseHeader
+		responseBody := SalesforceGetURLFromBody(requestURL, requestMethod, requestProxy, requestHeaders, requestParameter)
+		checkResponseBody := string(responseBody)
+
+		if strings.Contains(checkResponseBody, "fwuid") {
+			return responseBody
+		} else {
+			checkResponseBody := string(responseBody)
+			if strings.Contains(checkResponseBody, "window.location.href ='") {
+
+				re := regexp.MustCompile("window.location.href ='([^']+)")
+				getURLFromBody := re.FindString(checkResponseBody)
+				responseHeader := string(getURLFromBody[23:])
+				requestURL := responseHeader
+				responseBody := SalesforceGetURLFromBody(requestURL, requestMethod, requestProxy, requestHeaders, requestParameter)
+
+				if strings.Contains(checkResponseBody, "fwuid") {
+
+					return responseBody
+
+				} else {
+
+					responseHeader := SalesforceGetURLFromHeader(requestURL, requestMethod, requestProxy, requestHeaders, requestParameter)
+					requestURL := responseHeader
+					responseBody := SalesforceGetURLFromBody(requestURL, requestMethod, requestProxy, requestHeaders, requestParameter)
+					checkResponseBody := string(responseBody)
+					if strings.Contains(checkResponseBody, "fwuid") {
+
+						return responseBody
+					}
+				}
+			}
+		}
+	} else if strings.Contains(checkResponseBody, "window.location.href ='") {
+
+		re := regexp.MustCompile("window.location.href ='([^']+)")
+		getURLFromBody := re.FindString(checkResponseBody)
+		responseHeader := string(getURLFromBody[23:])
+		requestURL := responseHeader
+		responseBody := SalesforceGetURLFromBody(requestURL, requestMethod, requestProxy, requestHeaders, requestParameter)
+		checkResponseBody := string(responseBody)
+
+		if strings.Contains(checkResponseBody, "fwuid") {
+
+			return responseBody
+		} else {
+			responseHeader := SalesforceGetURLFromHeader(requestURL, requestMethod, requestProxy, requestHeaders, requestParameter)
+			requestURL := responseHeader
+			responseBody := SalesforceGetURLFromBody(requestURL, requestMethod, requestProxy, requestHeaders, requestParameter)
+			checkResponseBody := string(responseBody)
+			if strings.Contains(checkResponseBody, "fwuid") {
+
+				return responseBody
+			}
+		}
+	}
+
+	defer request.Body.Close()
+	return responseBody
+}
+
+//return request
+func SalesforceGetURLFromBody(requestURL string, requestMethod string, requestProxy string, requestHeaders []string, requestParameter map[string]string) []byte {
+
+	client := request.Client(requestProxy)
+	requestParam := url.Values{}
+
+	if requestParameter != nil {
+		for keyParameter, valuParameter := range requestParameter {
+
+			requestParam.Set(keyParameter, valuParameter)
+		}
+	}
+	//encode all param
+	finalRequestBody := bytes.NewBuffer([]byte(requestParam.Encode()))
+	request, err := http.NewRequest(requestMethod, requestURL, finalRequestBody)
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+	}
+
+	if requestHeaders != nil {
+
+		for _, header := range requestHeaders {
+			parts := strings.SplitN(header, ":", 2)
+			if len(parts) != 2 {
+				continue
+			}
+
+			request.Header.Set(parts[0], parts[1])
+		}
+	}
+
+	response, err := client.Do(request)
+
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+		gologger.Fatal().Msg("Can't send GET request (fromBody)")
+
+	}
+
+	responseBody, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatalln(err)
+		gologger.Fatal().Msg("Can't handle response (fromBody)")
+	}
+
+	defer request.Body.Close()
+	return responseBody
+}
+
+func SalesforceGetURLFromHeader(requestURL string, requestMethod string, requestProxy string, requestHeaders []string, requestParameter map[string]string) string {
+
+	client := request.Client(requestProxy)
+	requestParam := url.Values{}
+
+	if requestParameter != nil {
+		for keyParameter, valuParameter := range requestParameter {
+
+			requestParam.Set(keyParameter, valuParameter)
+		}
+	}
+	//encode all param
+	finalRequestBody := bytes.NewBuffer([]byte(requestParam.Encode()))
+	request, err := http.NewRequest(requestMethod, requestURL, finalRequestBody)
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+		gologger.Fatal().Msg("Can't send GET request (fromheader)")
+	}
+
+	if requestHeaders != nil {
+		for _, header := range requestHeaders {
+			parts := strings.SplitN(header, ":", 2)
+			if len(parts) != 2 {
+				continue
+			}
+			request.Header.Set(parts[0], parts[1])
+		}
+	}
+	response, err := client.Do(request)
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+
+	}
+
+	responseHeader := response.Header.Get("Location")
+	if err != nil {
+		log.Fatalln(err)
+		gologger.Fatal().Msg("Can't handle response (fromHeader)")
+
+	}
+
+	defer request.Body.Close()
+	return responseHeader
 }
