23ce75943cdef4c4a2b30d042b18f180323625dc
Ph33r <77519024+Ph33rr@users.noreply.github.com>
2022-07-11 15:48:15 +0300

    Update version.go

--- internal/runner/version.go
+++ internal/runner/version.go
@@ -1,12 +1,14 @@
 package runner
 
-
 import (
 	"fmt"
 	"os"
+
+	"github.com/logrusorgru/aurora"
 )
 
 func showVersion() {
-	fmt.Printf("CirrusGo %s\n", version)
+	fmt.Printf("\n[%s] CirrusGo %s\n", aurora.Green("VER").String(),
+		aurora.White(version).String())
 	os.Exit(2)
 }
