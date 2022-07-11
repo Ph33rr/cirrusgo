ee041d96720ad9bb86ece41692da682d7f75623c
Ph33r <77519024+Ph33rr@users.noreply.github.com>
2022-07-11 15:46:44 +0300

    Update constants.go

--- internal/runner/constants.go
+++ internal/runner/constants.go
@@ -1,71 +1,96 @@
 package runner
 
-
 const (
-version = "0.0.1"
-author  = "infosec_90"
+	version = "0.0.1"
+	author  = "infosec_90"
 
-banner = `
+	banner = `
    ______ _                           ______
   / ____/(_)_____ _____ __  __ _____ / ____/____
  / /    / // ___// ___// / / // ___// / __ / __ \
 / /___ / // /   / /   / /_/ /(__  )/ /_/ // /_/ /
-\____//_//_/   /_/    \__,_//____/ \____/ \____/ v0.0.1
-     v` + version + ` - @` + author
- // Version is the current version of CirrusGo`
+\____//_//_/   /_/    \__,_//____/ \____/ \____/ 
+     v` + version + ` - @` + author + ``
+	// Version is the current version of CirrusGo`
 
- usage = `
+	usage = `
  cirrusgo --help
  cirrusgo [app] [options]
  cirrusgo salesforce --help
- cirrusgo salesforce --payload options]
+ cirrusgo salesforce --payload options
  `
-
- options = `
- cirrusgo --help
-
+	applist = `
+     
+[#] salesforce
+`
+	optionss = `
 -u, --url <URL>           Define single URL to fuzz
--l, --list                Show App List
--c, --check               only check endpoint
+-l, --list                Show App List Support
+-c, --check               only check endpoint  Default with -u
 -H, --header <HEADER>     Pass custom header to target
 -proxy, --proxy <URL>     Use proxy to fuzz
 -V, --version             Show current CirrusGo version
 -h, --help                Display its help
  `
 
- salesforceOptions = `
+	salesforceOptions = `
  -u, --url <URL>           Define single URL 
  -c, --check               only check endpoint
  -lobj, --listobj          pull the object list.
- -obj --objects <NAME>     set the object name. Default value is "User" object.
+ -gobj --getobj            pull the object.
+ -obj --objects            set the object name. Default value is "User" object.
                              Juicy Objects: Case,Account,User,Contact,Document,Cont
                              entDocument,ContentVersion,ContentBody,CaseComment,Not
                              e,Employee,Attachment,EmailMessage,CaseExternalDocumen
                              t,Attachment,Lead,Name,EmailTemplate,EmailMessageRelation
- -re --recordid  <NAME>    set the recode id to dump the record
+ -gre --getrecord          pull the Record id.
+ -re --recordid            set the recode id to dump the record
+ -cw --chkWritable         check all Writable objects
  -f, --full                dump all pages of objects.
- -d --dump                 dump for manual
+ -d --dump                 dump Juicy File
  -H, --header <HEADER>     Pass custom header to target
  -proxy, --proxy <URL>     Use proxy to fuzz
  -o, --output <FILE>       File to save results
 
 [flaqs payload]
-command: cirrusgo salesforce --payload options
+ [command: cirrusgo salesforce --payload options]
+
 -payload --payload        Generator payload for test manual Default "ObjectList"
  [options]
 
 GetItems               -obj set object 
                           -page set page
                           -pages set pageSize
-GetRecord             -re set recoder id 
-WritableOBJ           -obj set object 
-SearchObj             -obj set object 
+GetRecord              -re set recoder id 
+WritableOBJ            -obj set object 
+SearchObj              -obj set object 
                           -page set page
                           -pages set pageSize
-AuraContext           -fwuid set UID 
-                         -App set AppName
-                         -markup set markup
+AuraContext            -fwuid set UID 
+                          -App set AppName
+                          -markup set markup
 ObjectList             -no options
 Dump                   -no options 
--h, --help              Display its help`
+-h, --help                Display its help`
+
+	salesforceOptionsPayload = `
+[flaqs payload]
+[command: cirrusgo salesforce --payload options]
+
+-payload --payload        Generator payload for test manual Default "ObjectList"
+[options]
+
+-GetItems               -obj set object 
+                         -page set page
+                         -pages set pageSize
+-GetRecord              -re set recoder id 
+-WritableOBJ            -obj set object 
+-SearchObj              -obj set object 
+                         -page set page
+                         -pages set pageSize
+-AuraContext            -fwuid set UID 
+                         -App set AppName
+                         -markup set markup
+-ObjectList             -no options
+-Dump                   -no options `
 )
