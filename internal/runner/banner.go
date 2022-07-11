0add6e146bd903abb83a8d497b148bd2cfc6fd66
Ph33r <77519024+Ph33rr@users.noreply.github.com>
2022-07-11 15:46:13 +0300

    Update banner.go

--- internal/runner/banner.go
+++ internal/runner/banner.go
@@ -1,6 +1,5 @@
 package runner
 
-
 import (
 	"github.com/projectdiscovery/gologger"
 	"github.com/projectdiscovery/gologger/levels"
@@ -8,8 +7,7 @@ import (
 
 func showBanner() {
 	gologger.DefaultLogger.SetMaxLevel(levels.LevelDebug)
-	gologger.Print().Msgf("%s\n", banner)
-	gologger.Print().Msgf("\t\ttwitter: infosec_90\n\n")
+	gologger.Print().Msgf("%s\n", banner+"\n")
 	gologger.Warning().Msg("Use with caution. You are responsible for your actions\n")
 	gologger.Warning().Msg("Developers assume no liability and are not responsible for any misuse or damage.\n")
 }
